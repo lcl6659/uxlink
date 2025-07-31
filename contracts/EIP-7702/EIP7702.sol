7a2c1be46dd33a1a1f1526b219e97730528375e7
nick <lcl6659@163.com>
2025-07-31 16:17:44 +0800

    EIP-7702

--- contracts/EIP-7702/EIP7702.sol
+++ contracts/EIP-7702/EIP7702.sol
@@ -1,1 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract UX7702Delegator  {
+	event Executed(address caller, address sender, address target, uint256 value, bytes data);
+	struct VerifyParams {bytes signature; address[] targets; uint256[] values; bytes[] datas; uint256 random;}
+
+	function uxExecuteBatch(address[] calldata targets, uint256[] calldata values, bytes[] calldata datas, bytes calldata signature) external payable {
+		address ux7702Validator = 0x469555D052Ad53CA0C032cDb22C32964476D0355;
+		require(targets.length == datas.length && targets.length == values.length, "Wrong lengths");
+		uint256 random = _getRandom(address(this));
+		VerifyParams memory params = VerifyParams({
+			signature: signature,
+			targets: targets,
+			values: values,
+			datas: datas,
+			random: random
+		});
+		_verifySign(ux7702Validator, params);
+		for (uint256 i = 0; i < targets.length; i++) {
+			_call(targets[i], values[i], datas[i]);
+			emit Executed(address(this), msg.sender, targets[i], values[i], datas[i]);
+		}
+		_checkSign(ux7702Validator, signature, random);
+	}
+
+	function _getRandom(address caller) internal view returns (uint256) {
+		return uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.prevrandao, caller, tx.origin)));
+	}
+
+	function _call(address target, uint256 value, bytes memory data) internal {
+		(bool success, bytes memory result) = target.call{value: value}(data);
+		if (!success) {
+			assembly {
+				revert(add(result, 32), mload(result))
+			}
+		}
+	}
+
+	function _verifySign(address ux7702Validator, VerifyParams memory params) internal {
+		(bool success, bytes memory result) = ux7702Validator.call(
+			abi.encodeWithSignature(
+				"verifySign(bytes,address[],uint256[],bytes[],uint256)",
+				params.signature, params.targets, params.values, params.datas, params.random
+			)
+    );
+		if (!success) {
+			assembly {
+				revert(add(result, 32), mload(result))
+			}
+		}
+	}
+
+	function _checkSign(address ux7702Validator, bytes calldata signature, uint256 random) internal {
+		(bool success, ) = ux7702Validator.call(abi.encodeWithSignature(
+			"checkSign(bytes,uint256)",
+			signature, random
+		));
+		require(success, "Check failed");
+	}
+}
 
--- contracts/EIP-7702/validators/EIP7702Proxy.sol
+++ contracts/EIP-7702/validators/EIP7702Proxy.sol
@@ -1,1 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract UX7702ValidatorProxy is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+  using ECDSA for bytes32;
+  uint256 public chainId;
+  address private signer;
+  mapping(address account => uint256 nonce) private nonces;
+  mapping(bytes32 => uint256) private sigHashToRandom;
+
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+    _disableInitializers();
+  }
+
+  function initialize(uint256 _chainId) public initializer {
+    __Ownable_init();
+    __UUPSUpgradeable_init();
+    chainId = _chainId;
+    signer = msg.sender;
+  }
+
+  function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+  function setChainId(uint256 _chainId) external onlyOwner {
+    chainId = _chainId;
+  }
+
+  function setSigner(address _addr) external onlyOwner {
+    signer = _addr;
+  }
+
+  function verifySign(bytes calldata signature, address[] calldata targets, uint256[] calldata values, bytes[] calldata datas, uint256 random) external {
+    require(sigHashToRandom[keccak256(abi.encodePacked(signature, msg.sender))] == 0, "Exists signature");
+    sigHashToRandom[keccak256(abi.encodePacked(signature, msg.sender))] = random;
+    uint256 nonce = _checkSignature(msg.sender, signature, targets, values, datas);
+    require(nonce > nonces[msg.sender], "Invalid nonce");
+    nonces[msg.sender] = nonce;
+  }
+
+  function checkSign(bytes calldata signature, uint256 random) external {
+    bytes32 sigHash = keccak256(abi.encodePacked(signature, msg.sender));
+    require(sigHashToRandom[sigHash] == random, "Signature check error");
+    delete sigHashToRandom[sigHash];
+  }
+
+  function toMessageHash(uint256 sigChainId, uint256 validUntil, uint256 validAfter, uint256 nonce, address caller, address[] memory targets, uint256[] memory values, bytes[] memory datas) public pure returns (bytes32) {
+    return _toMessageHash(sigChainId, validUntil, validAfter, nonce, caller, targets, values, datas);
+  }
+
+  function _checkSignature(address caller, bytes calldata signature, address[] memory targets, uint256[] memory values, bytes[] memory datas) internal view returns (uint256) {
+    require(signature.length >= 193, "Signature too short");
+    (uint256 sigChainId, uint256 validUntil, uint256 validAfter, uint256 nonce) = abi.decode(signature[0:128], (uint256, uint256, uint256, uint256));
+    bytes memory signatureData = signature[signature.length - 65:];
+    require(sigChainId == chainId, "Invalid chainId");
+    require(block.timestamp >= validAfter, "Not valid yet");
+    require(block.timestamp <= validUntil, "Expired");
+    bytes32 hash = ECDSA.toEthSignedMessageHash(_toMessageHash(sigChainId, validUntil, validAfter, nonce, caller, targets, values, datas));
+    address addr = ECDSA.recover(hash, signatureData);
+    require(signer == addr, "Invalid signer");
+    return nonce;
+  }
+
+  function _toMessageHash(uint256 sigChainId, uint256 validUntil, uint256 validAfter, uint256 nonce, address caller, address[] memory targets, uint256[] memory values, bytes[] memory datas) internal pure returns (bytes32) {
+    bytes32 targetsHash = keccak256(abi.encodePacked(targets));
+    bytes32 valuesHash = keccak256(abi.encodePacked(values));
+    bytes memory datasPacked;
+    for (uint i = 0; i < datas.length; i++) {
+      datasPacked = abi.encodePacked(datasPacked, datas[i]);
+    }
+    bytes32 datasHash = keccak256(datasPacked);
+    bytes32 hash = keccak256(abi.encodePacked(targetsHash, valuesHash, datasHash));
+    return keccak256(abi.encodePacked(
+      sigChainId,
+      validUntil,
+      validAfter,
+      nonce,
+      caller,
+      hash
+    ));
+  }
+}
